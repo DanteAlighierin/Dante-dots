707d0805d27d2bdaae70fe1d2a355007f2eebe7a
dante <zhivchikovv2@gmail.com>
2020-04-22 13:08:58 +0300

    rebuild st

--- config.h
+++ config.h
@@ -94,65 +94,32 @@ unsigned int tabspaces = 8;
 float alpha = 0.92;
 
 
- /* Terminal colors (16 first used in escape sequence) */
- static const char *colorname[] = {
-	  	/* 8 normal colors */
-	 -	"black",
-	 -	"red3",
-	 -	"green3",
-	 -	"yellow3",
-	 -	"blue2",
-	 -	"magenta3",
-	 -	"cyan3",
-	 -	"gray90",
-	 +	"#3b4252", /* black   */
-	 +	"#bf616a", /* red     */
-	 +	"#a3be8c", /* green   */
-	 +	"#ebcb8b", /* yellow  */
-	 +	"#81a1c1", /* blue    */
-	 +	"#b48ead", /* magenta */
-	 +	"#88c0d0", /* cyan    */
-	 +	"#e5e9f0", /* white   */
-	  
-	  	/* 8 bright colors */
-	 -	"gray50",
-	 -	"red",
-	 -	"green",
-	 -	"yellow",
-	 -	"#5c5cff",
-	 -	"magenta",
-	 -	"cyan",
-	 -	"white",
-	 +	"#4c566a", /* black   */
-	 +	"#bf616a", /* red     */
-	 +	"#a3be8c", /* green   */
-	 +	"#ebcb8b", /* yellow  */
-	 +	"#81a1c1", /* blue    */
-	 +	"#b48ead", /* magenta */
-	 +	"#8fbcbb", /* cyan    */
-	 +	"#eceff4", /* white   */
-	  
-	  	[255] = 0,
-		 
-		 	/* more colors can be added after 255 to use with DefaultXX */
-		-	"#cccccc",
-		-	"#555555",
-		+	"#2e3440", /* background */
-		+	"#d8dee9", /* foreground */
-		 };
 
 
 
+static const char *colorname[] = {
+  "#2e3440", /* base00 */
+  "#88c0d0", /* base08 */
+  "#bf616a", /* base0B */
+  "#5e81ac", /* base0A */
+  "#ebcb8b", /* base0D */
+  "#a3be8c", /* base0E */
+  "#d08770", /* base0C */
+  "#e5e9f0", /* base05 */
+  "#4c566a", /* base03 */
+  "#81a1c1", /* base09 */
+  "#3b4252", /* base01 */
+  "#434c5e", /* base02 */
+  "#d8dee9", /* base04 */
+  "#eceff4", /* base06 */
+  "#b48ead", /* base0F */
+  "#8fbcbb", /* base07 */
+};
 
-/*
- * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
- */
-unsigned int defaultfg = 259;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
-
+unsigned int defaultfg = 7;
+unsigned int defaultbg = 0;
+static unsigned int defaultcs = 13;
+static unsigned int defaultrcs = 0;
 /*
  * Default shape of cursor
  * 2: Block ("█")
