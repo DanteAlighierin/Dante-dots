cb65ba172cf7b62dd61a4e1c40e0a83852e6d213
Dante Alighierin <zhivchikovv2@gmail.com>
2020-05-16 16:20:23 +0300

    fish config

---
+++ fish/functions/fish_right_prompt.fish
@@ -0,0 +1,29 @@
+
+function get_git_status -d "Gets the current git status"
+  if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+    set -l dirty (command git status -s --ignore-submodules=dirty | wc -l | sed -e 's/^ *//' -e 's/ *$//' 2> /dev/null)
+    set -l ref (command git describe --tags --exact-match 2> /dev/null ; or command git symbolic-ref --short HEAD 2> /dev/null ; or command git rev-parse --short HEAD 2> /dev/null)
+
+    if [ "$dirty" != "0" ]
+      set_color -b normal
+      set_color B48EAD
+      echo "$dirty changed file"
+      if [ "$dirty" != "1" ]
+        echo "s"
+      end
+      echo " "
+      set_color -b B48EAD
+      set_color 2E2440
+    else
+      set_color -b 5E81AC
+      set_color ECEFF4
+    end
+
+    echo " $ref "
+    set_color normal
+   end
+end
+
+function fish_right_prompt -d "Prints right prompt"
+  get_git_status
+end
