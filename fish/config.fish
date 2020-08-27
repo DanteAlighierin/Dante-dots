f8cb04b82a7db0938ae59e4605b536c2db86e5ae
DanteAlighierin <49035889+DanteAlighierin@users.noreply.github.com>
2020-08-27 19:58:06 +0300

    fix start

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,6 @@
 set TTY1 (tty)
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
-    killall pulseaudio
-    killall autostart.sh
-    killall battery_notify.sh
+    killall autostart.sh	    
     # rm ~/.swayupdlog
     # touch ~/.swayupdlog
     rm ~/.sysupdlog
