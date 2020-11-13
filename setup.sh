7a066858023726eb3af5c3e8373c71a88cb790f2
DanteAlighierin <49035889+DanteAlighierin@users.noreply.github.com>
2020-11-13 14:28:26 +0000

    automatic packages install

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if grep 'void' /etc/*-release
+then sudo xbps-install -Su
+then sudo xbps-install mesa-intel-dri sway font-firacode fish neovim Waybar swaybg swaylock icecat chromium qtfm ranger cmus htop slurp grip telegram-desktop mako vlc wlsunset wireguard-tools
+fi
+
 for i in */; do rm -rf ~/.config/$i ; ln -sf $(pwd)/$i ~/.config ; done
