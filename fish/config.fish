e5855d136d99512e68b79c4f3210b6376cea2b00
Dante Alighierin <zhivchikovv2@gmail.com>
2020-12-30 16:20:39 +0000

    firefox webrender,wayland variables

--- fish/config.fish
+++ fish/config.fish
@@ -7,14 +7,25 @@ if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
     touch ~/.sysupdlog
     
 end
+
 set fish_greeting
-set -x XDG_RUNTIME_DIR /run/user/dante
+#chmod 0700 /tmp/swaytmp
+#set -x XDG_RUNTIME_DIR /run/user/dante
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
 set -x XDG_DATA_HOME $HOME/.local/share
 set -x XDG_CURRENT_DESKTOP Unity
 set -x MOZ_ENABLE_WAYLAND 1
-set -x QT_QPA_PLATFORMTHEME qt5ct
+set -x BEMENU_BACKEND wayland
+set -x XDG_SESSION_TYPE wayland
+set -x MOZ_ACCELERATED 1
+set -x MOZ_WEBRENDER 1
+set -x GDK_BACKEND wayland 
+set -x QT_QPA_PLATFORM wayland-egl 
+set -x SDL_VIDEODRIVER wayland
+set -x CLUTTER_BACKEND wayland
+
+
 # set -x QT_STYLE_OVERRIDE kvantum
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
     # dbus-launch --exit-with-session sway
