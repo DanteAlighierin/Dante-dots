6ca08b15464f2f060b1f348addf4ccdba585ac50
Dante Alighierin <zhivchikovv2@gmail.com>
2021-02-23 16:58:10 +0300

    aliases for term clear and pfetch

---
+++ fish/functions/cdcl.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function cdcl --wraps='cd ;cl' --description 'alias cdcl cd;cl'
+  cd ;cl $argv;
+end
---
+++ fish/functions/pfetch.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function pfetch --wraps=neofetch --description 'alias pfetch neofetch'
+  neofetch  $argv;
+end
