ef63b83aa9eb647abfa15f4f2547e0ab2029ddb2
Dante Alighierin <zhivchikovv2@gmail.com>
2021-02-23 16:57:06 +0300

    setup for new stuff

--- sway/bar.sh
+++ sway/bar.sh
@@ -16,12 +16,14 @@ current_time=$(date "+%H:%M")
 
 # Network
 
-ping=$(ping -c 1 www.google.es | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
+ping=$(ping -c 1 www.ya.ru | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
 
 
 # Others
 
 #language=$(swaymsg -t get_inputs | grep -m1 'xkb_active_layout_name' | cut -d '\"' -f4)
 language=$(swaymsg -t get_inputs | grep -m1 "xkb_active_layout_name" | cut -d '"' -f4)
+#language=$(swaymsg -t get_inputs | jq -r '.[] | select(.identifier == "1578:16641:MOSART_Semi._2.4G_Keyboard_Mouse") | .xkb_active_layout_name[0:3]')
+#language=$(swaymsg -t get_inputs -r | jq -r '.[] | select(.identifier | .identifier == "1578:16641:MOSART_Semi._2.4G_Keybord_Mouse") | .xkb_active_layout_name[0:3]')
 
 echo " ⌨ $language  ($ping ms) $date_and_week $current_time"
