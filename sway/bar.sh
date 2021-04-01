44b3d1565e40bc9da27a392eac93a76e3f941fda
Dante Alighierin <zhivchikovv2@gmail.com>
2021-04-01 17:02:14 +0300

    fix layout kb

--- sway/bar.sh
+++ sway/bar.sh
@@ -9,14 +9,15 @@ keyboard_input_name="1:1:AT_Translated_Set_2_keyboard"
 
 # Date and time
 
-date_and_week=$(date "+%Y.%m.%d ")
+#date_and_week=$(date "+%m.%d ")
 
-current_time=$(date "+%H:%M")
+current_time=$(date "+%a %F %H:%M")
 
 
 # Network
 
 ping=$(ping -c 1 www.ya.ru | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
+
 
 
 # Others
@@ -26,4 +27,4 @@ language=$(swaymsg -t get_inputs | grep -m1 "xkb_active_layout_name" | cut -d '"
 #language=$(swaymsg -t get_inputs | jq -r '.[] | select(.identifier == "1578:16641:MOSART_Semi._2.4G_Keyboard_Mouse") | .xkb_active_layout_name[0:3]')
 #language=$(swaymsg -t get_inputs -r | jq -r '.[] | select(.identifier | .identifier == "1578:16641:MOSART_Semi._2.4G_Keybord_Mouse") | .xkb_active_layout_name[0:3]')
 
-echo " ⌨ $language  ($ping ms) $date_and_week $current_time"
+echo " ⌨ $language  ($ping ms) $current_time"
